f1ea72419c2608c8e5447bc203ccf2296a18b127
reyfico <federicodesimone30@gmail.com>
2020-08-23 17:01:06 -0300

    primer push

---
+++ main.py
@@ -0,0 +1,20 @@
+#saludo = "Hola " 
+'''
+nombre = input("pone tu nombre:")
+print (saludo + nombre)
+'''
+num1 = int(input("pone el primer numero: "))
+num2 = int(input("pone el segundo numero: "))
+
+print (num1 + num2)
+
+
+
+
+
+
+
+
+
+
+
