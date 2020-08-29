68587bc8fdd5c4201afcaa2de472f8b24c97fd00
fede <federicodesimone30@gmail.com>
2020-08-29 18:18:37 -0300

    ejercicio de if

--- main.py
+++ main.py
@@ -2,11 +2,27 @@
 '''
 nombre = input("pone tu nombre:")
 print (saludo + nombre)
-'''
+
 num1 = int(input("pone el primer numero: "))
 num2 = int(input("pone el segundo numero: "))
 
 print (num1 + num2)
+
+'''
+
+
+num1 = int(input("pedir numero "))
+num2 = int(input("pedir numero "))
+num3 = int(input("pedir numero "))
+
+prom = (num1 + num2 + num3)/3
+
+print("el promedio es %.2f"%prom)
+if ( prom >= 6):
+	print("aprobado")
+
+else:
+	print("sos un burro")
 
 
 
