110b64e5e235bba4a0dd8dd9d896bf9512ddaba5
reyfico <federicodesimone30@gmail.com>
2020-11-04 16:23:27 -0300

    desde 0

---
+++ hola.py
@@ -0,0 +1,16 @@
+'''
+saludo = "hola mundo "
+nombre = input("ingresa tu nombre: ")
+
+print(saludo + nombre)
+
+'''
+
+
+num1 = int(input("ingrese el primer numero: "))
+num2 = int(input("ingrese el segundo numero: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
